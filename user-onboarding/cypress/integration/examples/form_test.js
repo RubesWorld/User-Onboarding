74cb00c009ebf2e8151e363e43cce8227d45ebe9
RubesWorld <ra15@outlook.com>
2020-12-17 14:40:46 -0800

    Email Test Complete

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -4,9 +4,16 @@ describe("User-Onboarding",()=>{
     });
 
     const nameInput = () => cy.get('input[name="name"]'); 
+    const emailInput = () => cy.get('input[name="email"]');
 
-     it('MVP', ()=>{
+
+     it('name check', ()=>{
         nameInput().type('Ruben')
         nameInput().should('have.value','Ruben')
      })
+     it('email check',()=>{
+         emailInput().type('ra15@outlook.com')
+        emailInput().should('have.value','ra15@outlook.com')
+     })
+
 })
